974c2916bd00f239129b53ca2205e35f66d61904
Okafor Daniel <105250983+Dancentcee@users.noreply.github.com>
2022-11-07 09:49:53 +0000

    Initial commit

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.gh
+  pkgs.heroku
+  pkgs.openssh_with_kerberos
+  pkgs.nodejs-16_x
 		pkgs.nodePackages.typescript-language-server
 		pkgs.yarn
 		pkgs.replitPackages.jest
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Protected from './Components/Layout/Protected';
 import Login from './Components/Pages/Login';
-import Users from './Components/Pages/Users';
+//import Users from './Components/Pages/Users';
 import NotFound from './Components/Pages/NotFound';
 import ErrorBoundary from './Components/ErrorBoundary';
 import NavBar from './Components/NavBar'
@@ -11,19 +11,19 @@ import NavBar from './Components/NavBar'
 
 export default function App() {
   return (
-   <Router>
-   <ErrorBoundary>
-    <div className="App">
-     <NavBar /> 
-   <Routes>
-   <Route index element={<Layout />}>
-     <Route path="/"> element={<Login />} />
-     <Route element={<Protected />}>
-    </Route>
-  <Route path="*" element={<NotFound />} />
-  </Routes>
-  </div>
-  </ErrorBoundary>
-  </Router>
-  );
+    <Router>
+      <ErrorBoundary>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route index element={<Layout />}>
+              <Route path="/"> element={<Login />} />
+                <Route element={<Protected />}>
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </ErrorBoundary>
+        </Router>
+        );
 }
